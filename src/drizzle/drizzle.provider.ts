1ed3c0cc99706494a66c677abf56bc693e56f2a0
Oleksandr <crm.31885@gmail.com>
2025-06-23 20:45:51 +0300

    fix: migrates

--- src/drizzle/drizzle.provider.ts
+++ src/drizzle/drizzle.provider.ts
@@ -2,7 +2,7 @@ import { drizzle, NodePgDatabase } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
 import * as schema from './schema';
 import { ConfigService } from '@nestjs/config';
-
+import * as dotenv from 'dotenv';
 export const DrizzleAsyncProvider = 'DrizzleAsyncProvider';
 
 export const drizzleProvider = [
@@ -12,8 +12,8 @@ export const drizzleProvider = [
     useFactory: (configService: ConfigService) => {
       // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
       // const connectionString = configService.get<string>('DATABASE_URL');
-      const connectionString =
-        'postgresql://postgres:postgres@localhost:5432/statistic?schema=public';
+      const connectionString = process.env.DATABASE_URL;
+      // 'postgresql://postgres:postgres@localhost:5432/statistic?schema=public';
       const pool = new Pool({
         // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
         connectionString,
